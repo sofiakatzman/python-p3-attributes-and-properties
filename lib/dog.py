10152f5c12baaa4dba67c9216d6231f9ef05ab3b
Sofia <katzmansof@gmail.com>
2023-05-18 16:28:33 -0400

    finished lab

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,39 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+
+    #initialization of properties along with default values to prevent error 
+    def __init__(self, name='Sofia', breed='Pointer'):
+        self.name = name
+        self.breed = breed
+
+    #get method for name  
+    def get_name(self): 
+        return self._name
+
+    #set method for name
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    
+    #property constructor method for name 
+    name = property(get_name, set_name)        
+
+
+    #get method for breed
+    def get_breed(self): 
+        return self._breed
+
+
+    #set method for breed
+    def set_breed(self, breed):
+        if breed in APPROVED_BREEDS: 
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")    
+
+
+    #property constructor method for breed
+    breed = property(get_breed, set_breed)
--- lib/person.py
+++ lib/person.py
@@ -17,3 +17,39 @@ APPROVED_JOBS = [
 
 class Person:
     pass
+
+    # initialize properties 
+    def __init__(self, name = "Sofia", job = "Admin"):
+        self.name = name
+        self.job = job
+
+    # set name method 
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    # get name method 
+    def get_name (self) :
+        return self._name 
+    
+    # name property constructor
+    name = property(get_name, set_name)
+
+    # set job method 
+    def set_job(self, job):
+        if job in APPROVED_JOBS:
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+
+    # get job method 
+    def get_job(self):
+        return self._job
+
+    # job property constructor 
+    job = property(get_job, set_job)
+
+
+
